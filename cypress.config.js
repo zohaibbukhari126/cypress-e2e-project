523914de848ee2e7f2b01b90ad77846a7eb30cc4
zohaibbukhari126 <zohaibbukhari126@gmail.com>
2026-05-21 10:58:37 +0500

    Fix folder structure - move files into cypress/e2e/

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,11 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
-    supportFile: false, 
+    specPattern: '{task1,task2}/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false,
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
-    screenshotOnRunFailure: true,
     defaultCommandTimeout: 10000,
     setupNodeEvents(on, config) {},
   },
