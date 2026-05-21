f76d6ca2097c9cae1857dff442f42cdd549894d7
Zohaib <89262530+zohaibbukhari126@users.noreply.github.com>
2026-05-21 10:43:20 +0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://www.saucedemo.com',
+    supportFile: false, 
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
