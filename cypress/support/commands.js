31ce126ffa183572aea9bb9fd9a396a0b05075bf
Zohaib <89262530+zohaibbukhari126@users.noreply.github.com>
2026-05-21 11:32:07 +0500

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,38 +1,8 @@
-
-
-
 Cypress.Commands.add('login', (username, password) => {
-  // Get the username input and type the provided username
-  cy.get('[data-test="username"]')
-    .should('be.visible')
-    .clear()
-    .type(username)
 
-  // Get the password input and type the provided password
-  cy.get('[data-test="password"]')
-    .should('be.visible')
-    .clear()
-    .type(password)
+    cy.get('[data-test="username"]').type(username)
 
-  // Click the login button to submit
-  cy.get('[data-test="login-button"]').click()
-})
+    cy.get('[data-test="password"]').type(password)
 
-
-Cypress.Commands.add('logout', () => {
-  // Open the hamburger menu
-  cy.get('#react-burger-menu-btn').click()
-
-  // Wait for menu to open, then click logout
-  cy.get('#logout_sidebar_link')
-    .should('be.visible')
-    .click()
-})
-
-
-
-Cypress.Commands.add('addItemToCart', (productSlug) => {
-  cy.get(`[data-test="add-to-cart-${productSlug}"]`)
-    .should('be.visible')
-    .click()
+    cy.get('[data-test="login-button"]').click()
 })
